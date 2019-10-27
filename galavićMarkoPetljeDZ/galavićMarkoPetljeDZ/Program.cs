5f3592f110f765e7eecc2217f444aa94a2b44073
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-10-27 19:34:43 +0100

    Add files via upload
    DZ

---
+++ galavićMarkoPetljeDZ/galavićMarkoPetljeDZ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoPetljeDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena;
+
+            Console.WriteLine("Unesi ocjenu");
+
+            ocjena = Console.ReadKey;
+
+           
+
+            for (int i = ocjena; i > 6; i++)
+            {
+                if(ocjena==1)
+                Console.WriteLine("1");
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
