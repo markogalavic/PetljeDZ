e0d3a02c9715609603e83afe47e6b9fee94df01a
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-11-02 12:20:18 +0100

    Add files via upload

---
+++ galavićMarkoPetljeDomaćaZadaća/galavićMarkoPetljeDomaćaZadaća/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoPetljeDomaćaZadaća
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            Console.WriteLine("Unesi broj ocijena");
+            broj = Int32.Parse(Console.ReadLine());
+            int i;
+          
+            for (i = 0; i <= broj; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 1)
+                {
+                    Console.WriteLine("Projek je 1");
+                }
+               
+            }
+         
+            Console.ReadKey();
+        }
+    }
+}
